55f2d33005b132ab97cfff9fe393be217295a99e
FXLaroche <fx.laroche@gmail.com>
2021-12-15 17:34:32 +0100

    created menu template and associated twig pages

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,11 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+// app.js
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+require('bootstrap-icons/font/bootstrap-icons.css');
