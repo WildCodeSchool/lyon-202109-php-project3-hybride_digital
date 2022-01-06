27a3dfe558d2d24ef1bca6d5d2b38999d91eb01e
RIOTTE <jcriotte69@gmail.com>
2022-01-06 20:33:21 +0100

    commit after correction profilType

--- src/Form/ProfilType.php
+++ src/Form/ProfilType.php
@@ -107,94 +107,100 @@ class ProfilType extends AbstractType
             ])
             ->add('precisTimeOfProspec', TextType::class, [
                 'label' => 'Si autre, pouvez-vous précisez ? '
-            ])
-            ->add('typeOfProspec', ChoiceType::class, [
-                'label' => 'Quel type de prospection mettez-vous en place ?',
-                'choices'  => [
-                    'Téléphone' => 1,
-                    'Mail' => 2,
-                    'LinkedIn' => 3,
-                    'Instagram' => 4,
-                    'Facebook' => 5,
-                    'Aucune' => 6,
-                ],
-                //'choice_label' => 'selector',
-                'multiple' => true,
-                'expanded' => true,
-                'by_reference' => false,
-            ])
-            ->add('prospecMoreClient', ChoiceType::class, [
-                'label' => 'Laquelle vous rapporte le plus de clients ? ',
-                'choices'  => [
-                    'téléphone' => 1,
-                    'Mail' => 2,
-                    'LinkedIn' => 3,
-                    'Instagram' => 4,
-                    'Facebook' => 5,
-                    'Aucune' => 6
-                ],
-                'multiple' => false,
-                'expanded' => true,
-                'by_reference' => false,
-            ])
-            ->add('numberClosPerMonth', ChoiceType::class, [
-                'label' => 'Combien de closing fetes-vous par mois ?',
-                'choices'  => [
-                    '1-2 contrats' => 1,
-                    '3-5 contrats' => 2,
-                    '5-10 contrats' => 3,
-                    '+10 contrats' => 4,
-                    'Autre' => 5,
-                ],
-            ])
-            ->add('precisClosPerMonth', TextType::class, [
-                'label' => 'Si autre, pouvez-vous précisez ? '
-            ])
-            ->add('budOfProspPerMonth', ChoiceType::class, [
-                'label' => 'Quel est le budget que vous investissez/mois dans la prospection ? ',
-                'choices'  => [
-                    '0-100 Euros' => 1,
-                    '100-200 Euros' => 2,
-                    '200-300 Euros' => 3,
-                    '300-500 Euros' => 4,
-                    '500-1K Euros' => 5,
-                    '+1K Euros' => 6,
-                    'Autre' => 7,
-                ],
-            ])
-            ->add('prcisBudProsMonth', TextType::class, [
-                'label' => 'Si autre, pouvez-vous précisez ? '
-            ])
-            ->add('analyseProspec', ChoiceType::class, [
-                'label' => 'Analysez-vous vos retours de campagne de prospection ? ',
-                'choices' => ['Oui' => 1, 'Non' => 2],
-                'multiple' => false,
-                'expanded' => true,
-                'by_reference' => false,
-                'required' => false,
-            ])
-            ->add('satisfiedOfRoi', ChoiceType::class, [
-                'label' => 'Etes-vous satisfait de votre ROI ? ',
-                'choices' => ['Oui' => 1, 'Non' => 2],
-                'multiple' => false,
-                'expanded' => true,
-                'by_reference' => false,
-                'required' => false,
-            ])
-            ->add('priorityCommercial', ChoiceType::class, [
-                'label' => 'Quelles sont vos priorités commerciales pour 12 mois à venir ?',
-                'choices'  => [
-                    'Générer le plus de leads' => 1,
-                    'Améliorer la satisfaction des clients' => 2,
-                    'Augmenter la rétention des clients' => 3,
-                    'Augmenter la notoriété de la marque' => 4,
-                    'Conclure plus de transactions' => 5,
-                    'Autre' => 6
-                ],
-                'multiple' => true,
-                'expanded' => true,
-                'by_reference' => false,
             ]);
+            $this->buildFormLastPart($builder);
+    }
+
+    public function buildFormLastPart(FormBuilderInterface $builder): void
+    {
+        $builder
+        ->add('typeOfProspec', ChoiceType::class, [
+            'label' => 'Quel type de prospection mettez-vous en place ?',
+            'choices'  => [
+                'Téléphone' => 1,
+                'Mail' => 2,
+                'LinkedIn' => 3,
+                'Instagram' => 4,
+                'Facebook' => 5,
+                'Aucune' => 6,
+            ],
+            //'choice_label' => 'selector',
+            'multiple' => true,
+            'expanded' => true,
+            'by_reference' => false,
+        ])
+        ->add('prospecMoreClient', ChoiceType::class, [
+            'label' => 'Laquelle vous rapporte le plus de clients ? ',
+            'choices'  => [
+                'téléphone' => 1,
+                'Mail' => 2,
+                'LinkedIn' => 3,
+                'Instagram' => 4,
+                'Facebook' => 5,
+                'Aucune' => 6
+            ],
+            'multiple' => false,
+            'expanded' => true,
+            'by_reference' => false,
+        ])
+        ->add('numberClosPerMonth', ChoiceType::class, [
+            'label' => 'Combien de closing fetes-vous par mois ?',
+            'choices'  => [
+                '1-2 contrats' => 1,
+                '3-5 contrats' => 2,
+                '5-10 contrats' => 3,
+                '+10 contrats' => 4,
+                'Autre' => 5,
+            ],
+        ])
+        ->add('precisClosPerMonth', TextType::class, [
+            'label' => 'Si autre, pouvez-vous précisez ? '
+        ])
+        ->add('budOfProspPerMonth', ChoiceType::class, [
+            'label' => 'Quel est le budget que vous investissez/mois dans la prospection ? ',
+            'choices'  => [
+                '0-100 Euros' => 1,
+                '100-200 Euros' => 2,
+                '200-300 Euros' => 3,
+                '300-500 Euros' => 4,
+                '500-1K Euros' => 5,
+                '+1K Euros' => 6,
+                'Autre' => 7,
+            ],
+        ])
+        ->add('prcisBudProsMonth', TextType::class, [
+            'label' => 'Si autre, pouvez-vous précisez ? '
+        ])
+        ->add('analyseProspec', ChoiceType::class, [
+            'label' => 'Analysez-vous vos retours de campagne de prospection ? ',
+            'choices' => ['Oui' => 1, 'Non' => 2],
+            'multiple' => false,
+            'expanded' => true,
+            'by_reference' => false,
+            'required' => false,
+        ])
+        ->add('satisfiedOfRoi', ChoiceType::class, [
+            'label' => 'Etes-vous satisfait de votre ROI ? ',
+            'choices' => ['Oui' => 1, 'Non' => 2],
+            'multiple' => false,
+            'expanded' => true,
+            'by_reference' => false,
+            'required' => false,
+        ])
+        ->add('priorityCommercial', ChoiceType::class, [
+            'label' => 'Quelles sont vos priorités commerciales pour 12 mois à venir ?',
+            'choices'  => [
+                'Générer le plus de leads' => 1,
+                'Améliorer la satisfaction des clients' => 2,
+                'Augmenter la rétention des clients' => 3,
+                'Augmenter la notoriété de la marque' => 4,
+                'Conclure plus de transactions' => 5,
+                'Autre' => 6
+            ],
+            'multiple' => true,
+            'expanded' => true,
+            'by_reference' => false,
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
