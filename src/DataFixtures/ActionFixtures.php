fe65b60d6c9d49c2deee8e52a3341bd649ed27e8
Anthony69600 <a.verchere69@gmail.com>
2022-02-04 15:02:46 +0100

    action_lorem

--- src/DataFixtures/ActionFixtures.php
+++ src/DataFixtures/ActionFixtures.php
@@ -11,30 +11,38 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 class ActionFixtures extends Fixture implements DependentFixtureInterface
 {
     public const ACTION = [
-        ['Création compte facebook', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-          Excepteur sint occaecat cupidatat non proident,
-           sunt in culpa qui officia deserunt mollit anim id est.', [0]],
-        ['Modification compte facebook', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          Excepteur sint occaecat cupidatat non proident,
-           sunt in culpa qui officia deserunt mollit anim id est labo', [1]],
-        ['Création compte instagram', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-        incididunt ut labore et dolore magna aliqua.
-         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-          Excepteur sint occaecat cupidatat non proident,
-           sunt in culpa qui officia deserunt mollit anim id est labo.', [2]],
-        ['Modification compte instagram', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod t
-        incididunt ut labore et dolore magna aliqua.
-         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-          Excepteur sint occaecat cupidatat non proident,
-           sunt in culpa qui officia deserunt mollit anim id est l.', [3]],
+        ['Création compte facebook', 'Pour créer un compte Facebook :
+        Accédez à facebook.com et cliquez sur Créer un compte.
+        Renseignez votre nom, votre adresse e-mail ou votre numéro de mobile, votre mot de passe, 
+        votre date de naissance et votre genre.
+        Cliquez sur Inscription.
+        Pour finaliser la création de votre compte, vous devez confirmer votre adresse e-mail ou votre numéro de mobile
+        .Si vous rencontrez des problèmes pour créer un compte Facebook :
+        Pour nous informer du problème que vous rencontrez lors de la création de votre compte Facebook, 
+        vous pouvez remplir ce formulaire.', [0]],
+        ['Modification compte facebook', 'Voici comment trouver vos paramètres :
+            Cliquez sur account en haut à droite de Facebook.
+            Sélectionnez Paramètres et confidentialité, puis cliquez sur Paramètres.
+            Cliquez sur le paramètre que vous souhaitez mettre à jour en choisissant
+            l’une des options dans la barre latérale gauche.
+            Les paramètres incluent les catégories suivantes :
+            Général : modifiez des informations de base, comme votre nom, votre nom d’utilisateur ou 
+            votre adresse e-mail.
+            Sécurité et connexion : modifiez votre mot de passe, puis activez les alertes 
+            et les autorisations afin de sécuriser votre compte.
+            Confidentialité : choisissez qui peut voir vos futures publications et qui peut vous trouver.
+            Profil et identification : choisissez qui peut voir votre journal et 
+            les publications dans lesquelles vous êtes identifié(e).
+            Blocage : gérez les éléments et les personnes à bloquer.
+            Langue et région : sélectionnez la langue et le format de date que vous souhaitez utiliser sur Facebook.', [1]],
+        ['Création compte instagram', "Rendez-vous sur l'App Store ou sur Google Play pour télécharger l'application
+         Instagram. Ouvrez l'application en cliquant sur l'icône. Sur la page d'accueil, cliquez sur S'inscrire avec un e-mail 
+        ou un numéro de téléphone ou sur Créer un compte.
+        Une boîte de dialogue s'affiche et demande l'autorisation d'accéder à vos contacts. Cliquez sur Refuser ou Autoriser.
+        Complétez le champ libre avec votre numéro de téléphone ou votre mail, puis cliquez sur le bouton Suivant.", [2]],
+        ['Modification compte instagram', "Ouvrez votre application Instagram en appuyant sur la célèbre icône multicolore représentant un appareil photo. Ensuite, dirigez-vous sur votre compte personnel. Pour ce faire, touchez la petite photo de profil qui se trouve tout en bas à droite de votre écran.
+        Une fois sur votre page, appuyez sur la mention « Modifier le profil » qui se trouve entre vos photos publiées et les informations de votre compte.
+        Sélectionnez alors la ligne « Username » sur laquelle est inscrit votre nom d'utilisateur actuel. Supprimez-le et écrivez le nouveau nom que vous souhaitez adopter pour votre compte. Instagram va valider ou non votre proposition, selon la disponibilité de votre choix.", [3]],
     ];
 
     public function load(ObjectManager $manager): void
